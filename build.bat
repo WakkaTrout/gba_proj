d19b79f2fc0c80de1e7bd36b2d0c8c8a064cdc88
yosmo78 <yosmo78@gmail.com>
2026-05-02 12:52:01 -0700

    look into

--- build.bat
+++ build.bat
@@ -1,4 +1,9 @@
 @echo off
 echo %DEVKITARM%
 
+::-mcpu=arm7tdmi -mtune=arm7tdmi
+::-fomit-frame-pointer
+::-ffast-math
+::-Wno-class-memaccess
+
 C:\devkitPro\devkitARM\bin\arm-none-eabi-gcc.exe -mthumb -mthumb-interwork -o3 -nostartfiles -T linker.ld -o game.elf game.c && C:\devkitPro\devkitARM\bin\arm-none-eabi-objcopy -O binary game.elf game.gba && rm game.elf
